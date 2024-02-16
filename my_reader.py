1620e1f3b0766be10b6a459f29056939b701641a
rkenski <rafael.kenski@gmail.com>
2024-02-16 15:49:23 -0300

    2

--- my_reader.py
+++ my_reader.py
@@ -101,177 +101,177 @@ def _convert_verb_indices_to_wordpiece_indices(verb_indices: List[int], offsets:
 
 
 
-@DatasetReader.register("srl-pt")
-class SrlReader(DatasetReader):
-    """
-    This dataset reader is almost identical to allennlp_models/syntax/srl/srl_reader.py
-    But the original file couldn't be used with xlmr models (at the time)
-    And the _read was changed to read folds data.
+# @DatasetReader.register("srl-pt")
+# class SrlReader(DatasetReader):
+#     """
+#     This dataset reader is almost identical to allennlp_models/syntax/srl/srl_reader.py
+#     But the original file couldn't be used with xlmr models (at the time)
+#     And the _read was changed to read folds data.
 
-    Parameters
-    ----------
-    token_indexers : ``Dict[str, TokenIndexer]``, optional
-        We similarly use this for both the premise and the hypothesis.  See :class:`TokenIndexer`.
-        Default is ``{"tokens": SingleIdTokenIndexer()}``.
-    domain_identifier: ``str``, (default = None)
-        A string denoting a sub-domain of the Ontonotes 5.0 dataset to use. If present, only
-        conll files under paths containing this domain identifier will be processed.
-    bert_model_name : ``Optional[str]``, (default = None)
-        The BERT model to be wrapped. If you specify a bert_model here, then we will
-        assume you want to use BERT throughout; we will use the bert tokenizer,
-        and will expand your tags and verb indicators accordingly. If not,
-        the tokens will be indexed as normal with the token_indexers.
+#     Parameters
+#     ----------
+#     token_indexers : ``Dict[str, TokenIndexer]``, optional
+#         We similarly use this for both the premise and the hypothesis.  See :class:`TokenIndexer`.
+#         Default is ``{"tokens": SingleIdTokenIndexer()}``.
+#     domain_identifier: ``str``, (default = None)
+#         A string denoting a sub-domain of the Ontonotes 5.0 dataset to use. If present, only
+#         conll files under paths containing this domain identifier will be processed.
+#     bert_model_name : ``Optional[str]``, (default = None)
+#         The BERT model to be wrapped. If you specify a bert_model here, then we will
+#         assume you want to use BERT throughout; we will use the bert tokenizer,
+#         and will expand your tags and verb indicators accordingly. If not,
+#         the tokens will be indexed as normal with the token_indexers.
 
-    Returns
-    -------
-    A ``Dataset`` of ``Instances`` for Semantic Role Labelling.
-    """
-    def __init__(self,
-                 token_indexers: Dict[str, TokenIndexer] = None,
-                 domain_identifier: str = None,
-                 lazy: bool = False,
-                 bert_model_name: str = None) -> None:
-        super().__init__(lazy)
-        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
-        self._domain_identifier = domain_identifier
-        self.xlm = "xlm" in bert_model_name
+#     Returns
+#     -------
+#     A ``Dataset`` of ``Instances`` for Semantic Role Labelling.
+#     """
+#     def __init__(self,
+#                  token_indexers: Dict[str, TokenIndexer] = None,
+#                  domain_identifier: str = None,
+#                  lazy: bool = False,
+#                  bert_model_name: str = None) -> None:
+#         super().__init__(lazy)
+#         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+#         self._domain_identifier = domain_identifier
+#         self.xlm = "xlm" in bert_model_name
         
-        self.bert_tokenizer = AutoTokenizer.from_pretrained(bert_model_name, use_fast = False)
+#         self.bert_tokenizer = AutoTokenizer.from_pretrained(bert_model_name, use_fast = False)
 
-        # the model class is different in xlmr models 
-        # the vocab is not in the same place as in bert
-        # so we create the vocab in the xlmr
-        if self.xlm:
-            self.vocab = {self.bert_tokenizer.convert_ids_to_tokens(i): i for i in range(250001)}
-            self.vocab.update(self.bert_tokenizer.added_tokens_encoder)
+#         # the model class is different in xlmr models 
+#         # the vocab is not in the same place as in bert
+#         # so we create the vocab in the xlmr
+#         if self.xlm:
+#             self.vocab = {self.bert_tokenizer.convert_ids_to_tokens(i): i for i in range(250001)}
+#             self.vocab.update(self.bert_tokenizer.added_tokens_encoder)
 
-        self.lowercase_input = False
+#         self.lowercase_input = False
         
-    def _wordpiece_tokenize_input(self, tokens: List[str]) -> Tuple[List[str], List[int], List[int]]:
-        """
-        Convert a list of tokens to wordpiece tokens and offsets, as well as adding
-        BERT CLS and SEP tokens to the begining and end of the sentence.
+#     def _wordpiece_tokenize_input(self, tokens: List[str]) -> Tuple[List[str], List[int], List[int]]:
+#         """
+#         Convert a list of tokens to wordpiece tokens and offsets, as well as adding
+#         BERT CLS and SEP tokens to the begining and end of the sentence.
 
-        A slight oddity with this function is that it also returns the wordpiece offsets
-        corresponding to the _start_ of words as well as the end.
+#         A slight oddity with this function is that it also returns the wordpiece offsets
+#         corresponding to the _start_ of words as well as the end.
 
-        We need both of these offsets (or at least, it's easiest to use both), because we need
-        to convert the labels to tags using the end_offsets. However, when we are decoding a
-        BIO sequence inside the SRL model itself, it's important that we use the start_offsets,
-        because otherwise we might select an ill-formed BIO sequence from the BIO sequence on top of
-        wordpieces (this happens in the case that a word is split into multiple word pieces,
-        and then we take the last tag of the word, which might correspond to, e.g, I-V, which
-        would not be allowed as it is not preceeded by a B tag).
+#         We need both of these offsets (or at least, it's easiest to use both), because we need
+#         to convert the labels to tags using the end_offsets. However, when we are decoding a
+#         BIO sequence inside the SRL model itself, it's important that we use the start_offsets,
+#         because otherwise we might select an ill-formed BIO sequence from the BIO sequence on top of
+#         wordpieces (this happens in the case that a word is split into multiple word pieces,
+#         and then we take the last tag of the word, which might correspond to, e.g, I-V, which
+#         would not be allowed as it is not preceeded by a B tag).
 
-        For example:
+#         For example:
 
-        `annotate` will be bert tokenized as ["anno", "##tate"].
-        If this is tagged as [B-V, I-V] as it should be, we need to select the
-        _first_ wordpiece label to be the label for the token, because otherwise
-        we may end up with invalid tag sequences (we cannot start a new tag with an I).
+#         `annotate` will be bert tokenized as ["anno", "##tate"].
+#         If this is tagged as [B-V, I-V] as it should be, we need to select the
+#         _first_ wordpiece label to be the label for the token, because otherwise
+#         we may end up with invalid tag sequences (we cannot start a new tag with an I).
 
-        Returns
-        -------
-        wordpieces : List[str]
-            The BERT wordpieces from the words in the sentence.
-        end_offsets : List[int]
-            Indices into wordpieces such that `[wordpieces[i] for i in end_offsets]`
-            results in the end wordpiece of each word being chosen.
-        start_offsets : List[int]
-            Indices into wordpieces such that `[wordpieces[i] for i in start_offsets]`
-            results in the start wordpiece of each word being chosen.
-        """
-        word_piece_tokens: List[str] = []
-        end_offsets = []
-        start_offsets = []
-        cumulative = 0
-        for token in tokens:
-            if self.xlm:
-                word_pieces = self.bert_tokenizer._tokenize(token)
-            else:
-                word_pieces = self.bert_tokenizer.wordpiece_tokenizer.tokenize(token)
-            start_offsets.append(cumulative + 1)
-            cumulative += len(word_pieces)
-            end_offsets.append(cumulative)
-            word_piece_tokens.extend(word_pieces)
+#         Returns
+#         -------
+#         wordpieces : List[str]
+#             The BERT wordpieces from the words in the sentence.
+#         end_offsets : List[int]
+#             Indices into wordpieces such that `[wordpieces[i] for i in end_offsets]`
+#             results in the end wordpiece of each word being chosen.
+#         start_offsets : List[int]
+#             Indices into wordpieces such that `[wordpieces[i] for i in start_offsets]`
+#             results in the start wordpiece of each word being chosen.
+#         """
+#         word_piece_tokens: List[str] = []
+#         end_offsets = []
+#         start_offsets = []
+#         cumulative = 0
+#         for token in tokens:
+#             if self.xlm:
+#                 word_pieces = self.bert_tokenizer._tokenize(token)
+#             else:
+#                 word_pieces = self.bert_tokenizer.wordpiece_tokenizer.tokenize(token)
+#             start_offsets.append(cumulative + 1)
+#             cumulative += len(word_pieces)
+#             end_offsets.append(cumulative)
+#             word_piece_tokens.extend(word_pieces)
 
-        return word_piece_tokens, end_offsets, start_offsets
+#         return word_piece_tokens, end_offsets, start_offsets
 
-    @overrides
-    def _read(self, file_path: str) -> Iterable[Instance]:
-        # This reads the folds data
+#     @overrides
+#     def _read(self, file_path: str) -> Iterable[Instance]:
+#         # This reads the folds data
         
-        # if `file_path` is a URL, redirect to the cache
-        file_path = cached_path(file_path)
-        logger.info("Reading SRL instances from dataset files at: %s", file_path)
+#         # if `file_path` is a URL, redirect to the cache
+#         file_path = cached_path(file_path)
+#         logger.info("Reading SRL instances from dataset files at: %s", file_path)
 
-        with codecs.open(file_path, 'r', encoding='utf8') as open_file:
-            for line in open_file:
-                words, tags = line.split('\t')
-                words, tags = words.split(' '), tags.strip().split(' ')
-                verb_indicator = [1 if label[-2:] == "-V" else 0 for label in tags]
-                tokens = [Token(t) for t in words]
-                yield self.text_to_instance(tokens, verb_indicator, tags)
+#         with codecs.open(file_path, 'r', encoding='utf8') as open_file:
+#             for line in open_file:
+#                 words, tags = line.split('\t')
+#                 words, tags = words.split(' '), tags.strip().split(' ')
+#                 verb_indicator = [1 if label[-2:] == "-V" else 0 for label in tags]
+#                 tokens = [Token(t) for t in words]
+#                 yield self.text_to_instance(tokens, verb_indicator, tags)
                 
-    def text_to_instance(self,  # type: ignore
-                         tokens: List[Token],
-                         verb_label: List[int],
-                         tags: List[str] = None) -> Instance:
-        """
-        We take `pre-tokenized` input here, along with a verb label.  The verb label should be a
-        one-hot binary vector, the same length as the tokens, indicating the position of the verb
-        to find arguments for.
-        """
-        # pylint: disable=arguments-differ
-        metadata_dict: Dict[str, Any] = {}
-        if self.bert_tokenizer is not None:
-            wordpieces, offsets, start_offsets = self._wordpiece_tokenize_input([t.text for t in tokens])
+#     def text_to_instance(self,  # type: ignore
+#                          tokens: List[Token],
+#                          verb_label: List[int],
+#                          tags: List[str] = None) -> Instance:
+#         """
+#         We take `pre-tokenized` input here, along with a verb label.  The verb label should be a
+#         one-hot binary vector, the same length as the tokens, indicating the position of the verb
+#         to find arguments for.
+#         """
+#         # pylint: disable=arguments-differ
+#         metadata_dict: Dict[str, Any] = {}
+#         if self.bert_tokenizer is not None:
+#             wordpieces, offsets, start_offsets = self._wordpiece_tokenize_input([t.text for t in tokens])
             
-            if self.xlm:
-                wordpieces = ["<s>"] + wordpieces + ["</s>"]
-            else:
-                wordpieces = ["[CLS]"] + wordpieces + ["[SEP]"]
-            new_verbs = _convert_verb_indices_to_wordpiece_indices(verb_label, offsets)
-            metadata_dict["offsets"] = start_offsets
-            # In order to override the indexing mechanism, we need to set the `text_id`
-            # attribute directly. This causes the indexing to use this id.
-            if self.xlm: 
-                text_field = TextField([Token(t, text_id=self.vocab[t]) for t in wordpieces],
-                                   token_indexers=self._token_indexers)
-            else:
-                text_field = TextField([Token(t, text_id=self.bert_tokenizer.vocab[t]) for t in wordpieces],
-                                   token_indexers=self._token_indexers)
+#             if self.xlm:
+#                 wordpieces = ["<s>"] + wordpieces + ["</s>"]
+#             else:
+#                 wordpieces = ["[CLS]"] + wordpieces + ["[SEP]"]
+#             new_verbs = _convert_verb_indices_to_wordpiece_indices(verb_label, offsets)
+#             metadata_dict["offsets"] = start_offsets
+#             # In order to override the indexing mechanism, we need to set the `text_id`
+#             # attribute directly. This causes the indexing to use this id.
+#             if self.xlm: 
+#                 text_field = TextField([Token(t, text_id=self.vocab[t]) for t in wordpieces],
+#                                    token_indexers=self._token_indexers)
+#             else:
+#                 text_field = TextField([Token(t, text_id=self.bert_tokenizer.vocab[t]) for t in wordpieces],
+#                                    token_indexers=self._token_indexers)
             
-            verb_indicator = SequenceLabelField(new_verbs, text_field)
+#             verb_indicator = SequenceLabelField(new_verbs, text_field)
 
-        else:
-            text_field = TextField(tokens, token_indexers=self._token_indexers)
-            verb_indicator = SequenceLabelField(verb_label, text_field)
+#         else:
+#             text_field = TextField(tokens, token_indexers=self._token_indexers)
+#             verb_indicator = SequenceLabelField(verb_label, text_field)
 
-        fields: Dict[str, Field] = {}
-        fields['tokens'] = text_field
-        fields['verb_indicator'] = verb_indicator
+#         fields: Dict[str, Field] = {}
+#         fields['tokens'] = text_field
+#         fields['verb_indicator'] = verb_indicator
 
-        if all([x == 0 for x in verb_label]):
-            verb = None
-            verb_index = None
-        else:
-            verb_index = verb_label.index(1)
-            verb = tokens[verb_index].text
+#         if all([x == 0 for x in verb_label]):
+#             verb = None
+#             verb_index = None
+#         else:
+#             verb_index = verb_label.index(1)
+#             verb = tokens[verb_index].text
 
-        metadata_dict["words"] = [x.text for x in tokens]
-        metadata_dict["verb"] = verb
-        metadata_dict["verb_index"] = verb_index
+#         metadata_dict["words"] = [x.text for x in tokens]
+#         metadata_dict["verb"] = verb
+#         metadata_dict["verb_index"] = verb_index
 
-        if tags:
-            if self.bert_tokenizer is not None:
-                new_tags = _convert_tags_to_wordpiece_tags(tags, offsets)
-                fields['tags'] = SequenceLabelField(new_tags, text_field)
-            else:
-                fields['tags'] = SequenceLabelField(tags, text_field)
-            metadata_dict["gold_tags"] = tags
-        fields["metadata"] = MetadataField(metadata_dict)
-        return Instance(fields)
+#         if tags:
+#             if self.bert_tokenizer is not None:
+#                 new_tags = _convert_tags_to_wordpiece_tags(tags, offsets)
+#                 fields['tags'] = SequenceLabelField(new_tags, text_field)
+#             else:
+#                 fields['tags'] = SequenceLabelField(tags, text_field)
+#             metadata_dict["gold_tags"] = tags
+#         fields["metadata"] = MetadataField(metadata_dict)
+#         return Instance(fields)
 
 
 @DatasetReader.register("simple")
